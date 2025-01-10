d5a9ceb0519c8df8e8d246fa4f41b33d5e3d8feb
Júlia Serra Trujillo <juliaserratrujillo@gmail.com>
2025-01-10 20:16:48 +0100

    Store data in lists implemented

--- Delivery3_Heatmap/Assets/_Scripts/Delivery3/VisualizationLogic.cs
+++ Delivery3_Heatmap/Assets/_Scripts/Delivery3/VisualizationLogic.cs
@@ -9,34 +9,38 @@ using UnityEngine.Networking;
 
 public class VisualizationLogic : MonoBehaviour
 {
-    List<Vector3> data;
+    List<Vector3> posData;
+    List<Vector3> attackData;
+    List<Vector3> deathData;
+    List<Vector3> hitData;
+    List<Vector3> killData;
 
     public void ShowPositionData()
     {
-        StartCoroutine(RetrieveData("positionDownloader"));
+        StartCoroutine(RetrieveData("positionDownloader", posData));
     }
 
     public void ShowAttackData()
     {
-        StartCoroutine(RetrieveData("attackDownloader"));
+        StartCoroutine(RetrieveData("attackDownloader", attackData));
     }
 
     public void ShowDeathData()
     {
-        StartCoroutine(RetrieveData("deathDownloader"));
+        StartCoroutine(RetrieveData("deathDownloader", deathData));
     }
 
     public void ShowHitData()
     {
-        StartCoroutine(RetrieveData("hitDownloader"));
+        StartCoroutine(RetrieveData("hitDownloader", hitData));
     }
 
     public void ShowKillData()
     {
-        StartCoroutine(RetrieveData("killDownloader"));
+        StartCoroutine(RetrieveData("killDownloader", killData));
     }
 
-    public IEnumerator RetrieveData(string fileName)
+    public IEnumerator RetrieveData(string fileName, List<Vector3> data)
     {
         WWWForm form = new WWWForm();
         //form.AddField("x", position.x.ToString(CultureInfo.InvariantCulture));
@@ -55,9 +59,37 @@ public class VisualizationLogic : MonoBehaviour
             Debug.Log($"Succcess: {www.downloadHandler.text}");
             //lastPlayerID = uint.Parse(www.downloadHandler.text);
             //callback.Invoke(lastPlayerID);
+
+            StoreData(www.downloadHandler.text, data);
         }
         yield return null;
     }
 
+    private void StoreData(string json, List<Vector3> data)
+    {
+        data.Clear();
+        
+        string formattedJson = json.TrimStart('[').TrimEnd(']');
+
+        string[] jsonOBJs = formattedJson.Split(new[] { "}," }, StringSplitOptions.None);
+
+        foreach (string jsonOBJ in jsonOBJs)
+        {
+            try
+            {
+                string fixedJson = jsonOBJ.Trim();
+                if (!fixedJson.EndsWith("}")) fixedJson += "}";
+
+                Vector3 pos = JsonUtility.FromJson<Vector3>(fixedJson);
+                data.Add(pos);
+
+                //Debug.Log(pos);
+            }
+            catch (ArgumentException e)
+            {
+                Debug.LogError($"Failed: {jsonOBJ}\nException: {e.Message}");
+            }
+        }
+    }
 }
 #endif
